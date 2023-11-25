b225ea89e64c325d0cb9b6f261d26544daa08fe1
yogita <yogitarathod1204@gmail.com>
2023-11-25 15:42:16 +0530

    song.js added

---
+++ spotify_backend/routes/song.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const Song = require("../models/song");
+
+router.post(
+    "/create",passport.authenticate("jwt",{session : false }),
+async(req,res) =>{
+    const {name, thumbnail, track} = req.body;
+    if(!name || !thumbnail || !track){
+        return res
+        .status(301)
+        .json({err:"insufficient details to create song."});
+    }
+    const artist = req.user._id;
+    const songDetails = {name, thumbnail, track, artist};
+    const createSong = await Song.create(songDetails);
+    return res.status(200).json(createSong);
+
+};
+router.get{
+    "/get/mysongs",
+    passport.authenticate("jwt",{session : false}),
+    async(req,res) => {
+        const currentuser = req.user;
+        const songs = await Song.find({artist : req.user._id});
+        return res.status(200).json({data : songs});
+    }
+};
+
+module.exports = router;
